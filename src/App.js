1dc2134a94b6253f7654dcc181d5c3dd93ada06e
noahemmenegger <noahemmenegger.business@gmail.com>
2022-09-08 11:36:41 +0200

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
             <h1>Mixed Verb Forms Quiz</h1>
+            <table>
+                <Button value="test" />
+            </table>
         </div>
     );
 }
